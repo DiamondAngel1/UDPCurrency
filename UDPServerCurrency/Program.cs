0f78f614c5787b52db40506124fbcfbba4591832
DiamondAngel1 <tararukolga78@gmail.com>
2025-05-28 17:54:10 +0300

    Second HW

--- UDPServerCurrency/Program.cs
+++ UDPServerCurrency/Program.cs
@@ -9,6 +9,9 @@ UdpClient udpServer = new UdpClient(2004);
 Console.WriteLine("Сервер запущено на порту 2004...");
 
 HashSet<IPEndPoint> clients = new();
+Dictionary<IPEndPoint, int> clientRequests = new();
+const int maxRequests = 3;
+TimeSpan resetTime = TimeSpan.FromMinutes(1);
 
 Dictionary<string, decimal> exchangeRates = new()
 {
@@ -21,19 +24,45 @@ Dictionary<string, decimal> exchangeRates = new()
 };
 
 IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+Dictionary<IPEndPoint, DateTime> blockedClients = new();
 
 while (true){
     byte[] receivedBytes = udpServer.Receive(ref remoteEndPoint);
     string request = Encoding.UTF8.GetString(receivedBytes).Trim();
 
-    if (!clients.Contains(remoteEndPoint)){
+    if (blockedClients.ContainsKey(remoteEndPoint) && DateTime.UtcNow < blockedClients[remoteEndPoint] + resetTime){
+        string blockedMessage = $"Ви перевищили ліміт запитів. Ви заблоковані до {blockedClients[remoteEndPoint] + resetTime} | Час: {DateTime.Now}";
+        udpServer.Send(Encoding.UTF8.GetBytes(blockedMessage), remoteEndPoint);
+        continue;
+    }
+    else if (blockedClients.ContainsKey(remoteEndPoint)&& DateTime.UtcNow >= blockedClients[remoteEndPoint]+resetTime){
+        blockedClients.Remove(remoteEndPoint);
+        clientRequests[remoteEndPoint] = 0;
+        Console.WriteLine($"Клієнт {remoteEndPoint} розблокований | Час: {DateTime.UtcNow}");
+    }
+    if (!clients.Contains(remoteEndPoint))
+    {
         clients.Add(remoteEndPoint);
-        Console.WriteLine($"Клієнт підключився: {remoteEndPoint} | Час: {DateTime.Now}");
+        Console.WriteLine($"Клієнт підключився: {remoteEndPoint} | Час: {DateTime.UtcNow}");
     }
 
     Console.WriteLine($"Запит від {remoteEndPoint}: {request}");
 
     string response;
+    if (!clientRequests.ContainsKey(remoteEndPoint)){
+        clientRequests[remoteEndPoint] = 1;
+    }
+    else{
+        clientRequests[remoteEndPoint]++;
+    }
+    if (clientRequests[remoteEndPoint] > maxRequests){
+        response = $"Ви перевищили ліміт запитів ({maxRequests}). Ви заблоковані на {resetTime.TotalMinutes} хвилину";
+        blockedClients[remoteEndPoint] = DateTime.UtcNow;
+        Console.WriteLine($"Клієнт {remoteEndPoint} перевищив ліміт запитів | Час блокування {DateTime.UtcNow}");
+        udpServer.Send(Encoding.UTF8.GetBytes(response), remoteEndPoint);
+        continue;
+    }
+
     if (exchangeRates.ContainsKey(request)){
         response = $"Курс {request}: {exchangeRates[request]}";
     }
@@ -48,6 +77,6 @@ while (true){
     }
     Console.WriteLine($"Відправлено {remoteEndPoint}: {response}");
     byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-    udpServer.Send(responseBytes, responseBytes.Length, remoteEndPoint);
+    udpServer.Send(responseBytes, remoteEndPoint);
 }
 udpServer.Close();
